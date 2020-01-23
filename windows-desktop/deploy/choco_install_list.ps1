b321c061a99edee1152ba6cb1007f4eb7b018de0
Robert Depke <github@satele.de>
2020-01-23 11:09:44 +0100

    update software list

--- windows-desktop/deploy/choco_install_list.ps1
+++ windows-desktop/deploy/choco_install_list.ps1
@@ -1,4 +1,4 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+# Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 #system
 choco install powershell -y
@@ -15,16 +15,6 @@ choco install git-lfs -y
 choco install firefox -y
 choco install googlechrome -y
 
-#devops
-choco install vagrant -y
-choco install virtualbox -y
-
-#dev
-choco install golang -y
-setx GOPATH D:\repos\go
-setx PATH=%PATH%;%GOPATH%\bin
-choco install make -y
-
 #gaming
 choco install steam -y
 choco install goggalaxy -y
@@ -32,6 +22,7 @@ choco install uplay -y
 choco install origin -y
 
 #misc
+choco install signal -y
 choco install keepass -y
 choco install irfanview -y
 choco install sumatrapdf -y
